fd3dc761c66fc156c1a13402107b8ec4bb675a94
tanem <melikyan.mac@gmail.com>
2026-09-22 17:23:46 +0300

    Enlarge mobile LED illustration and refine title typography

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "@fontsource/cormorant-garamond/500.css";
 import "@fontsource/cormorant-garamond/500-italic.css";
+import "@fontsource/cormorant-garamond/700-italic.css";
 import "@fontsource/cormorant-garamond/600.css";
 import "@fontsource/manrope/400.css";
 import "@fontsource/manrope/500.css";
